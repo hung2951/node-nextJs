4ff41eaef80c7984db0267918f4040ed59f404b8
taquangnghia <nghiatqph13235@fpt.edu.vn>
2022-07-27 10:19:26 +0700

    update-category(controler)

--- routes/category.js
+++ routes/category.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { create, list, remove, upload } from "../controllers/category";
+import { create, list, read, remove, upload } from "../controllers/category";
 const router = Router();
 router.post('/category',create)
 router.get('/category',list)
 router.delete('/category/:id',remove)
 router.patch('/category/:id',upload)
+router.get('/category/:id',read)
 export default router;
